184da0d39d43a8b272cab67b47933b85857d0c94
Adedamola Rioland <riolandadedamola@gmail.com>
2022-08-29 21:51:43 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {useEffect} from 'react'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import '../styles/globals.css'
-import '../styles/register.css'
+// import '../styles/register.css'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
--- pages/client.js
+++ pages/client.js
@@ -1,37 +1,52 @@
-import Link from "next/link";
-import { Fragment } from "react";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { useRouter } from "next/router";
-import app from "../firbase/firebaseAuth";
-const auth = getAuth(app);
-auth;
-const Client = () => {
-  var route = useRouter();
+// import Link from "next/link";
+// import { Fragment } from "react";
+// import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+// import { useRouter } from "next/router";
+// import app from "../firbase/firebaseAuth";
 
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      route.push("/login");
-    } else {
-      return (
-        <div>
-          client
-          <button
-            onClick={() => {
-              signOut(auth)
-                .then(() => {
-                  // Sign-out successful.
-                })
-                .catch((error) => {
-                  // An error happened.
-                });
-            }}
-          >
-            logout
-          </button>
-        </div>
-      );
-    }
-  });
-};
+// const auth = getAuth(app);
+// auth;
+// const Client = () => {
+//   var route = useRouter();
 
-export default Client;
+//   return (
+//     <div>
+//       client
+//       <button
+//         onClick={() => {
+//           signOut(auth)
+//             .then(() => {
+//               // Sign-out successful.
+//             })
+//             .catch((error) => {
+//               // An error happened.
+//             });
+//         }}
+//       >
+//         logout
+//       </button>
+//     </div>
+//   );
+// };
+
+
+
+// export async function getServerSideProps(context) {
+  
+//   onAuthStateChanged(auth, (user) => {
+//     if (user==null) {
+//       return {
+//         redirect: {
+//           permanent: false,
+//           destination: `/login`
+//         },
+//       };
+    
+//     }
+//   });
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   }
+// }
+
+// export default Client;
